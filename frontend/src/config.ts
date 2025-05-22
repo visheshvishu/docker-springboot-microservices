df5053af52c8532d6e13a849ab7482ad739dade4
vishesh <visheshg5050@gmail.com>
2025-05-22 13:26:14 +0530

    Delete frontend directory

--- frontend/src/config.ts
+++
@@ -1,8 +0,0 @@
-// API Gateway URL
-export const API_URL = 'http://localhost:8083';
-
-// Service-specific URLs (if needed for direct access)
-export const AUTH_URL = 'http://localhost:8082';
-export const CUSTOMER_URL = 'http://localhost:8085';
-export const ORDER_URL = 'http://localhost:8084';
-export const ADMIN_URL = 'http://localhost:8086'; 
